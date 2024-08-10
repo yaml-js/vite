cd61bdb0110c1b99d12ebc99f92c33c20a0e3dd2
Pedro Gomes <pedro.gomes@talkdesk.com>
2024-08-10 10:15:02 +0100

    fix: changes from configuring repo to be case insensitive

---
+++ packages/sample-react-app/src/app.tsx
@@ -0,0 +1,13 @@
+import config from './configs/app.yml'
+
+function App() {
+  console.log("config", config)
+
+  return (
+      <div>
+        <h1>React App with Yaml Support</h1>
+      </div>
+  )
+}
+
+export default App
